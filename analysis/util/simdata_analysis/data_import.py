3a9e6bbe243a8652c182c00f13e7ff22d13862cd
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 10:51:03 +0000

    Adding tarfile member sanitization to extractall()

--- analysis/util/simdata_analysis/data_import.py
+++ analysis/util/simdata_analysis/data_import.py
@@ -361,7 +361,26 @@ class BatchSimDataImporter(object):
         print('Input data file {0} does not exist.'.format(input_file_str))
         return None
       with tarfile.open(input_file_str, 'r') as inputfile:
-        inputfile.extractall(folder_str)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(inputfile, folder_str)
 
     # Load the input data.
     self.input_data = {}
--- lib/python/batch_sim/client.py
+++ lib/python/batch_sim/client.py
@@ -673,7 +673,26 @@ class BatchSimClient(object):
 
     with os_util.ChangeDir(worker_dir):
       with tarfile.open('configs.tar.gz', 'r:gz') as f:
-        f.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(f)
 
       worker_env = os.environ.copy()
       worker_env['MAKANI_HOME'] = worker_dir
